7d7f7656b889a774e50fa3951d0c762098c1e315
ggg <103683646+nadashvili@users.noreply.github.com>
2025-01-06 12:31:40 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const products = [
-    { id: 1, name: 'მობილური ტელეფონი', price: 299.99, description: 'კარგი ხარისხის კამერით მაღალი ხარისხის მობილური ტელეფონი.', category: 'ელექტრონიკა', image: 'https://imageslidergenerator.shahmirfaisal.com/slider?image=https%3A%2F%2Fi.ibb.co%2FrGTR525%2F1000021257.jpg&image=https%3A%2F%2Fi.ibb.co%2FfkYLbS7%2F1000021258.jpg&image=https%3A%2F%2Fi.ibb.co%2FxL0mt7V%2F1000021259.jpg&image=https%3A%2F%2Fi.ibb.co%2FXzydCb0%2F1000021260.jpg&image=https%3A%2F%2Fi.ibb.co%2Ff0VDTXY%2F1000021261.jpg&animationType=slide&autoPlay=false&radioButtonType=square&radioButtonSize=47&radioButtonGap=10&arrowsType=arrow-ios&arrowsBackground=hidden&arrowsBackgroundVisibility=222&arrowsSize=32&arrowsOffset=2&arrowsColor=%23666666' },
-    { id: 2, name: 'სარეცხი მანქანა', price: 399.99, description: 'სრულად ავტომატური სარეცხი მანქანა მოწინავე ფუნქციებით.', category: 'სანტექნიკა', image: 'https://i.postimg.cc/mrX9ZD9C/Picsart-25-01-05-22-30-47-935.jpg', sellerPhone: '123-456-7891' },
+    { id: 1, name: 'მობილური ტელეფონი', price: 299.99, description: 'კარგი ხარისხის კამერით მაღალი ხარისხის მობილური ტელეფონი.', category: 'ელექტრონიკა', image: 'https://shorturl.at/w8w9e2F1000021257.jpg&image=https%3A%2F%2Fi.ibb.co%2FfkYLbS7%2F1000021258.jpg&image=https%3A%2F%2Fi.ibb.co%2FxL0mt7V%2F1000021259.jpg&image=https%3A%2F%2Fi.ibb.co%2FXzydCb0%2F1000021260.jpg&image=https%3A%2F%2Fi.ibb.co%2Ff0VDTXY%2F1000021261.jpg&animationType=slide&autoPlay=false&radioButtonType=square&radioButtonSize=47&radioButtonGap=10&arrowsType=arrow-ios&arrowsBackground=hidden&arrowsBackgroundVisibility=222&arrowsSize=32&arrowsOffset=2&arrowsColor=%23666666' },
+    { id: 2, name: 'სარეცხი მანქანა', price: 399.99, description: 'სრულად ავტომატური სარეცხი მანქანა მოწინავე ფუნქციებით.', category: 'სანტექნიკა', image: 'https://shorturl.at/w8w9e', sellerPhone: '123-456-7891' },
     { id: 3, name: 'ელექტრული გიტარა', price: 499.99, description: 'პროფესიონალური ელექტრული გიტარა განსაკუთრებული ხმით.', category: 'მუსიკალური', image: 'https://imageslidergenerator.shahmirfaisal.com/slider?image=https%3A%2F%2Fi.ibb.co%2FzxXBtZK%2F1000020799.jpg&animationType=slide&autoPlay=true&radioButtonType=none&radioButtonSize=20&radioButtonGap=10&arrowsType=none&arrowsBackground=hidden&arrowsBackgroundVisibility=212&arrowsSize=46&arrowsOffset=1&arrowsColor=%23fff', sellerPhone: '123-456-7892' },
 ];
 
@@ -9,7 +9,8 @@ function renderProducts(productsList) {
     grid.innerHTML = ''; // წაშალოს არსებული პროდუქცია
 
     productsList.forEach(product => {
-        const productCard = document.createElement('div');
+        const productCard = document.createElement('div');https://imageslidergenerator.shahmirfaisal.com/slider?image=https%3A%2F%2Fi.ibb.co%2FrGTR525%2F1000021257.jpg&image=https%3A%2F%2Fi.ibb.co%2FfkYLbS7%2F1000021258.jpg&image=https%3A%2F%2Fi.ibb.co%2FxL0mt7V%2F1000021259.jpg&image=https%3A%2F%2Fi.ibb.co%2FXzydCb0%2F1000021260.jpg&image=https%3A%2F%2Fi.ibb.co%2Ff0VDTXY%2F1000021261.jpg&animationType=slide&autoPlay=false&radioButtonType=square&radioButtonSize=47&radioButtonGap=10&arrowsType=arrow-ios&arrowsBackground=hidden&arrowsBackgroundVisibility=222&arrowsSize=32&arrowsOffset=2&arrowsColor=%23666666' },
+    { id: 2, name: 'სარეცხი მანქანა', price: 399.99, description: 'სრულად ავტომატური სარეცხი მანქანა 
         productCard.classList.add('product-card');
         productCard.innerHTML = `
             <iframe src="${product.image}" frameborder="0" width="100%" height="200"></iframe>
